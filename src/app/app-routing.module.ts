05af8e5d4aaac2b4f52f5e071a2de337458377a7
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-26 00:00:43 -0500

    Fix login routing to render login component at the root path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule} from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./auth/login/login.component";
 
 const routes: Routes = [
+    {
+        path: '',
+        component: LoginComponent
+    },
     {
         path: 'home',
         component: HomeComponent
