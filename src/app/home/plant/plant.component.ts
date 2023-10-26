73496b0db4b60de7fc7fa4130bcc1b6d9d07fae6
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-26 18:41:54 -0500

    Implement "View plant details of a given garden" feature

--- src/app/home/plant/plant.component.ts
+++ src/app/home/plant/plant.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {PlantService} from "../../service/plant.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-plant',
   templateUrl: './plant.component.html',
   styleUrls: ['./plant.component.css']
 })
-export class PlantComponent {
+export class PlantComponent implements OnInit{
   plant: any;
+
+  constructor(private plantService: PlantService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap
+        .subscribe( (params) => {
+          let plantInd: string = params.get("plantId") || "";
+          // Get the parent route's paramMap
+          let gardenId: string = this.route.parent?.snapshot.paramMap.get("gardenId") || "";
+          this.plantService.getSinglePlant(gardenId,plantInd)
+              .subscribe({
+                  next: (response) => {
+                      console.log(response.message);
+                      console.log(response.data);
+                      this.plant = response.data;
+                  },
+                  error: (error) => {
+                      console.log("Plant not found", error);
+                  }
+              })
+        })
+  }
 
 }
