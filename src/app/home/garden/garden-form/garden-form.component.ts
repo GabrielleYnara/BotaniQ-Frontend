cb215623609282151eec56d8aae79566ca69a066
Gabrielle Ynara <gabrielleynara@ymail.com>
2023-10-26 13:27:57 -0500

    Implement garden-request.model.ts

--- src/app/home/garden/garden-form/garden-form.component.ts
+++ src/app/home/garden/garden-form/garden-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {GardenService} from "../../../service/garden.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-garden-form',
@@ -9,9 +11,23 @@ export class GardenFormComponent {
   description: string = "";
   notes: string = "";
 
+  constructor(private gardenService: GardenService,
+              private router: Router) {
+  }
   onSubmit() {
-    if ( this.description && this.description ){
-      console.log(this.description + " and " + this.notes);
+    if ( this.description ){
+      this.gardenService.createGarden(this.description, this.notes)
+          .subscribe({
+              next: (response) => {
+                console.log("Garden created!", response);
+                this.router.navigate(['/home']);
+              },
+              error: (error) => {
+                console.log("Garden creation failed!", error);
+              }
+          })
+    } else {
+      console.log("Description ")
     }
   }
 }
